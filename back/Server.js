d2e20a69cb4a9832bec0280b54a8978a07182bb3
Srisankar2001 <srisank-se20028@stu.kln.ac.lk>
2024-08-03 13:11:50 +0530

    finished

--- back/Server.js
+++ back/Server.js
@@ -18,15 +18,19 @@ const io = new Server(server,{
 
 
 io.on("connection", (socket) => {
-    // console.log(`User Connected: ${socket.id}`);
+    console.log(`User Connected: ${socket.id}`);
   
     socket.on("join_room",(data)=>{
         socket.join(data)
         console.log(`User join with user ID : ${socket.id}, room ID : ${data}`)
     })
-    // socket.on("disconnect", () => {
-    //   console.log(`User Disconnected: ${socket.id}`);
-    // });
+
+    socket.on("send_message",(data)=>{
+        socket.to(data.roomId).emit("receive_message" , data)
+    })
+    socket.on("disconnect", () => {
+      console.log(`User Disconnected: ${socket.id}`);
+    });
   });
 
 server.listen(3001,(err)=>{
